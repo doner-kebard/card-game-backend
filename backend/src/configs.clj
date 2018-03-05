643603622c8d29779f400e6a7ca521f783960581
Masclins <masclins@gmail.com>
2018-03-05 21:51:19 +0100

    New ini-hand to close #40 (#48)
    * changed initial power
    
    * changed initial power

--- backend/src/configs.clj
+++ backend/src/configs.clj
@@ -12,16 +12,16 @@
 
 (defn ini-hand
   "Generates de initial hand (can't be empty)"
-  [] (vec (repeat 12 {:power 10})))
-;  [{:power 5}
-;   {:power 6}
-;   {:power 7}
-;   {:power 8}
-;   {:power 9}
-;   {:power 10}
-;   {:power 11}
-;   {:power 12}
-;   {:power 13}
-;   {:power 14}
-;   {:power 15}
-;   {:power 16}])
+  []
+  [{:power 5}
+   {:power 6}
+   {:power 7}
+   {:power 8}
+   {:power 9}
+   {:power 10}
+   {:power 11}
+   {:power 12}
+   {:power 13}
+   {:power 14}
+   {:power 15}
+   {:power 16}]) 
