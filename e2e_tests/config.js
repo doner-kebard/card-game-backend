2fbeb2e45291b692bb36c5cac8bb885622d1c625
kenan-rhoton <kenan.rhoton@gmail.com>
2018-03-22 15:30:45 +0100

    Extracted server configuration to be outside of the Git repository (#93)

--- e2e_tests/config.js
+++ e2e_tests/config.js
@@ -3,5 +3,8 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 
 const config = yaml.safeLoad(fs.readFileSync('/configs/config.yml', 'utf8'));
-
+config.servers = {
+    frontend: "frontend:8880",
+    backend: "backend:3000",
+}
 module.exports = config;
--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,1 +1,22 @@
-module.exports = require('./data/config.yml');
+config = require('./data/config.yml')
+
+// Load server data with defaults if file doesn't exist
+try {
+    servers = require('./data/servers.yml')
+} catch(error) {
+
+    // Don't want to silently catch other errors
+    if (error.code !== 'MODULE_NOT_FOUND') {
+        throw error;
+    }
+
+    // Defaults to dev values
+    servers = {
+        servers: {
+            frontend: 'frontend:8880',
+            backend: 'backend:3000'
+        }
+    }
+}
+
+module.exports = Object.assign(config,servers);
