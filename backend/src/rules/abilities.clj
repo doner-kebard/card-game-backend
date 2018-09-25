32794d76569efb2301d5200034781a0a3c037cfc
amasclans <albert.masclans@globalzitro.com>
2018-09-25 09:15:20 +0200

    new ability

--- backend/src/rules/abilities.clj
+++ backend/src/rules/abilities.clj
@@ -4,10 +4,27 @@
   "Creates the ability function"
   [increase]
   (fn    
-;    "Alters a cards' power, by adding the a defined value to it.
+;    "Alters a card's power, by adding the a defined value to it.
 ;    This power may be negative, resulting in a decrease"
     [game-state play]
     (update-in
       game-state
       [:cards (:target play) :power]
       #(+ % increase))))
+
+(defn type-add-power
+  "Creates the ability function"
+  [row-type increase]
+  (fn
+;    "When played in a row-type row,
+;    alters a its power, by adding the a defined value to it.
+;    This power may be negative, resulting in a decrease"
+    [game-state play]
+    (if (= (get-in game-state [:rows (:row-id play) :type])
+           row-type)
+      (update-in
+        game-state
+        [:cards (:card-id play) :power]
+        #(+ % increase))
+
+      game-state)))
--- backend/test/rules/abilities_test.clj
+++ backend/test/rules/abilities_test.clj
@@ -16,3 +16,25 @@
     (get-in ((ability/add-power -13) {:cards [{}{:power 3}]} {:target 1 :card-id 91 :something-else "whatever"})
             [:cards 1 :power])))
 
+(defexpect type-add-power
+
+  ; Does nothing when shouldn't
+  (expect
+    42
+    (get-in ((ability/type-add-power "invi" 10) {:cards [{:power 42}] :rows [{}]} {:row-id 0 :card-id 0})
+            [:cards 0 :power]))
+  (expect
+    99
+    (get-in ((ability/type-add-power "invi" 10) {:cards [{}{:power 99}] :rows [{}{:type "type-0"}]} {:row-id 1 :card-id 1})
+            [:cards 1 :power]))
+  
+  ; Adds power when should
+  (expect
+    52
+    (get-in ((ability/type-add-power "invi" 10) {:cards [{:power 42}] :rows [{:type "invi"}]} {:row-id 0 :card-id 0})
+            [:cards 0 :power]))
+
+  (expect
+    -2
+    (get-in ((ability/type-add-power "vivi" -3) {:cards [{:power 42} {:power 1}] :rows [{}{}{:type "vivi"}]} {:row-id 2 :card-id 1})
+            [:cards 1 :power])))
