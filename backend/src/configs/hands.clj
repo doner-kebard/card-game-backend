c8a4df84525c7e3afa45adddf736ac164460c51d
amasclans <albert.masclans@globalzitro.com>
2018-10-01 14:33:13 +0200

    new default

--- backend/src/configs/hands.clj
+++ backend/src/configs/hands.clj
@@ -5,12 +5,12 @@
   "Default hand (can't be empty)"
   [{:power 8}
    {:power 7}
-   {:power 6 :ability (ability/type-add-power "overpower" 1)}
-   {:power 5 :ability (ability/type-add-power "overpower" 3)}
-   {:power 6 :ability (ability/add-power 1)}
-   {:power 6 :ability (ability/add-power -1)}
-   {:power 4 :ability (ability/add-power 2)}
-   {:power 4 :ability (ability/add-power -2)}])
+   (merge {:power 6} (ability/type-add-power "overpower" 1))
+   (merge {:power 5} (ability/type-add-power "overpower" 3))
+   (merge {:power 6} (ability/add-power 1))
+   (merge {:power 6} (ability/add-power -1))
+   (merge {:power 4} (ability/add-power 2))
+   (merge {:power 4} (ability/add-power -2))])
 
 (def default-hands
   "Default hands"
--- backend/src/configs/rows.clj
+++ backend/src/configs/rows.clj
@@ -2,8 +2,8 @@
 
 
 (def default-rows
-  [{:limit 4}
-   {:limit 4}
-   {:limit 4 :type "overpower"}
-   {:limit 4}
-   {:limit 4}])
+  [{:limit 3 :type "overpower"}
+   {:limit 3}
+   {:limit 3}
+   {:limit 3}
+   {:limit 3}])
