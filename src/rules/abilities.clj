c9386999cc11aa7175043baca584e3e0c94f9837
amasclans <albert.masclans@globalzitro.com>
2018-10-04 10:28:49 +0200

    tests

--- src/rules/abilities.clj
+++ src/rules/abilities.clj
@@ -38,6 +38,36 @@
                     (str "Weaken " (- increase) " on " row-type)
                     (str "Enhance " increase " on " row-type))})
 
+(defn ^:private row-affinity
+  [row-type increase]
+  (fn
+;   "When played in a row-type row,
+;   alters a its power, by adding the a defined value to it.
+;   This power may be negative, resulting in a decrease"
+    [game-state play]
+    (if (= (get-in game-state [:rows (:row-id play) :type])
+           row-type)
+      (update-in
+        game-state
+        [:cards (:card-id play) :power]
+        #(+ % increase))
+      game-state)))
+
+(defn ^:private strengthen
+  [increase]
+  (fn    
+;   "Alters a cards' power, by adding the a defined value to it.
+;   This power may be negative, resulting in a decrease"
+    [game-state play]
+    (update-in
+      game-state
+      [:cards (:target play) :power]
+      #(+ % increase))))
+
+(defn ^:private weaken
+  [decrease]
+  (strengthen (- decrease)))
+
 (defn generate-ability-fn
   "Creates an ability function from its description"
   [ability-description]
