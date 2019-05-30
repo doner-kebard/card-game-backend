72a348bd72ecbd7d42fa763815c3389661708537
Albert Masclans <albert.masclans@globalzitro.com>
2019-05-30 16:31:34 +0200

    body in body

--- src/api/handler.clj
+++ src/api/handler.clj
@@ -18,21 +18,21 @@
 
 (def^:private app-routes
   (compojure/routes
-    (compojure/POST "/lobby" [] (api/create-game))
+    (compojure/POST "/lobby" [] {:body (api/create-game)})
     (compojure/POST "/games/:id{[0-9]+}"
                     [id]
-                    (api/add-player (parse-int id)))
+                    {:body (api/add-player (parse-int id))})
     (compojure/GET "/games/:id{[0-9]+}/player/:player"
                    [id player]
-                   (api/get-game (parse-int id) player))
+                   {:body (api/get-game (parse-int id) player)})
     (compojure/POST "/games/:id{[0-9]+}/player/:player"
                     [id player :as {body :body}]
-                    (play-action (parse-int id) player body))
+                    {:body (play-action (parse-int id) player body)})
     (route/not-found "<h1>Page not found</h1>")))
 
 (defn ^:private handler
   [request]
-  {:body (app-routes request)})
+  (app-routes request))
 
 (def entry
   (middleware/wrap-json-response handler))
