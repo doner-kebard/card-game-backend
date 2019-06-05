2d1f72e92c23a714c68270a0a2430a8311798613
Masclins <albert.masclans@globalzitro.com>
2019-06-05 12:58:49 +0200

    Cleanup (#23)
    * remove pacts
    
    * better eastwood
    
    * added handler tests
    
    * added game-status messages
    
    * test-refresh solved

--- src/api/base.clj
+++ src/api/base.clj
@@ -30,7 +30,7 @@
 
 (defn ^:private create-empty-game
   "Creates a new instance of a game lobby"
-  [ini-config]
+  []
   (let [game-id (persistence/next-id)]
     (persistence/save-game
       {:lobby-status messages/no-opp
@@ -56,9 +56,8 @@
 
 (defn create-game
   "Creates a new instance of a game with a player"
-  ([] (create-game {}))
-  ([ini-config]
-   (let [lobby (create-empty-game ini-config)]
-     (-> lobby
-         (assoc :player-id (first (:player-ids lobby)))
-         (dissoc :player-ids)))))
+  []
+  (let [lobby (create-empty-game)]
+    (-> lobby
+        (assoc :player-id (first (:player-ids lobby)))
+        (dissoc :player-ids))))
